4fea736dbabf1580568b14c83b9fa46c99ced0ed
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-08-23 11:06:31 +0530

    2

--- src/main/scala/mushtaq/BankAccount.scala
+++ src/main/scala/mushtaq/BankAccount.scala
@@ -4,13 +4,15 @@ class BankAccount {
 
   private var _balance = 0
 
-  def deposit(amount: Int): Unit = {
+  def deposit(amount: Int): Unit = synchronized {
     _balance += amount
   }
 
-  def withdraw(amount: Int): Unit = {
+  def withdraw(amount: Int): Unit = synchronized {
     _balance -= amount
   }
 
-  def balance: Int = _balance
+  def balance: Int = synchronized {
+    _balance
+  }
 }
--- src/test/scala/mushtaq/BankAccountTest.scala
+++ src/test/scala/mushtaq/BankAccountTest.scala
@@ -1,15 +1,31 @@
 package mushtaq
 
-import org.scalatest.FunSuite
+import java.util.concurrent.{ExecutorService, Executors}
 
-class BankAccountTest extends FunSuite {
+object BankAccountTest {
+  val account = new BankAccount
 
-  test("asdasd") {
-    val account = new BankAccount
+  def main(args: Array[String]): Unit = {
+    val executorService = Executors.newScheduledThreadPool(1000)
 
-    account.deposit(100)
-    account.withdraw(100)
+    val runnable: Runnable = { () =>
+      account.deposit(100)
+    }
+
+    val runnable2: Runnable = { () =>
+      account.withdraw(100)
+    }
+
+    (1 to 10000).foreach { _ =>
+      executorService.submit(runnable)
+    }
+
+    (1 to 10000).foreach { _ =>
+      executorService.submit(runnable2)
+    }
+
+    Thread.sleep(5000)
+
     println(account.balance)
   }
-
 }
