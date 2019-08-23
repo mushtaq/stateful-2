08ce55166eea1af214645852a9da7f5bbde93986
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-08-23 15:29:06 +0530

    8

---
+++ src/main/scala/mushtaq/ProductService.scala
@@ -0,0 +1,45 @@
+package mushtaq
+
+import java.util.concurrent.Executors
+import java.util.function.Consumer
+
+class ProductService {
+  val executorService = Executors.newSingleThreadExecutor()
+
+  val data: Map[String, Int] = Map("car" -> 100, "rocket" -> 800)
+
+  def onPrice(productId: String, callback: Consumer[Int]): Unit = {
+    val op: Runnable = () => callback.accept(data(productId))
+    executorService.submit(op)
+  }
+
+  def totalPriceSequential(id1: String, id2: String, callback: Consumer[Int]) = {
+    onPrice(id1, price1 => {
+      onPrice(id2, price2 => {
+        callback.accept(price1 + price2)
+      })
+    })
+  }
+
+  def totalPricePar(id1: String, id2: String, callback: Consumer[Int]) = {
+    var total = 0
+    onPrice(id1, price => {
+      if (total == 0) {
+        total += price
+      } else {
+        callback.accept(total + price)
+      }
+    })
+    onPrice(id2, price => {
+      if (total == 0) {
+        total += price
+      } else {
+        callback.accept(total + price)
+      }
+    })
+  }
+
+  onPrice("car", price => println(price))
+  totalPriceSequential("car", "rocket", sum => println(sum))
+
+}
