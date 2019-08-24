ac40e056211d654a79b1d83c342f5a4b839c7df8
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-08-24 12:03:01 +0530

    4

--- src/test/scala/mushtaq/BankAccountTest.scala
+++ src/test/scala/mushtaq/BankAccountTest.scala
@@ -2,35 +2,35 @@ package mushtaq
 
 import java.util.concurrent.{ExecutorService, Executors}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.DurationDouble
 
 object BankAccountTest {
   val account = new BankAccount
 
+  val executorService = Executors.newScheduledThreadPool(1000)
   def main(args: Array[String]): Unit = {
-    val executorService               = Executors.newScheduledThreadPool(1000)
     implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(executorService)
 
-    val runnable: Runnable = { () =>
+    val depositFutures = (1 to 50000).map { _ =>
       account.deposit(100)
     }
 
-    val runnable2: Runnable = { () =>
+    val withdrawFutures = (1 to 50000).map { _ =>
       account.withdraw(100)
     }
 
-    (1 to 50000).foreach { _ =>
-      executorService.submit(runnable)
-    }
+    val resultF = Future
+      .sequence(depositFutures ++ withdrawFutures)
+      .flatMap { _ =>
+        account.balance.map { x =>
+          println(x)
+        }
+      }
 
-    (1 to 50000).foreach { _ =>
-      executorService.submit(runnable2)
-    }
+    Await.result(resultF, 1000.seconds)
 
-    Thread.sleep(4000)
+    executorService.shutdown()
 
-    account.balance.foreach { x =>
-      println(x)
-    }
   }
 }
